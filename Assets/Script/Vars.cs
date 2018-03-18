54e53ad079c1d46fa21b31cb5e890b8e641bf013
minomu3409 <37399789+minomu3409@users.noreply.github.com>
2018-03-18 17:06:35 +0900

    Initial commit

---
+++ Assets/Script/Vars.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Vars : Object {
+public static class Vars{
+
+	public static Vector3 refPosi = new Vector3 ();
+	public static Vector3 refAngle = new Vector3 ();
+	public static Vector3 camPosi = new Vector3 ();
+	public static Vector3 camAngle = new Vector3 ();
+	public static bool camMoveFlag = false;
+	public static bool camZoomed = false;
+
+//	// camera movable detection
+//	public static bool camMovable ()
+//	{
+//		return (!camMoveFlag && !camZoomed);
+//	}
+
+	// correction of refAngle
+	public static void refAngCorrection ()
+	{
+		Vector3 corr = refAngle - camAngle;
+		for (int i = 0; i < 3; i++) {
+			if (corr [i] < -180)
+				refAngle [i] += 360;
+			else if (corr [i] > 180)
+				refAngle [i] -= 360;
+		}
+	}
+
+}
